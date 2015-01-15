cbaefb91c590e693cf25423bc8ded57ce9d9be94
la002769 <la002769@etu.unice.fr>
2015-01-15 17:57:08 +0100

    Client side Android premiere version

---
+++ Android/app/src/main/java/com/polytech/cluedo/Remote.java
@@ -0,0 +1,109 @@
+package com.polytech.cluedo;
+
+/**
+ * Created by Alexia on 09/01/2015.
+ */
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
+
+public class Remote {
+
+    private static volatile Remote instance;
+    private static SocketIO socket;
+    public static String url;
+    public static String pseudo;
+    public static Context context;
+    public static boolean already_connect;
+    public static long mon_id;
+
+    private static final String MY_ID = "myId";
+
+    private static final String ADD_PLAYER = "addPlayer";
+
+
+    final IOCallback ioCallback = new IOCallback() {
+
+        @Override
+        public void onConnect() {
+            System.out.println("Am i here ?");
+            if (already_connect) {
+                socket.emit("reconnect", mon_id);
+            } else {
+                System.out.println("Where am i ?");
+                socket.emit(ADD_PLAYER, pseudo);
+                already_connect = true;
+            }
+            // Login to Waiting
+            Intent intent = new Intent(context, WaitingActivity.class);
+            intent.putExtra("MESSAGE", R.string.waitingForPlayers);
+            context.startActivity(intent);
+        }
+
+        @Override
+        public void onMessage(String s, IOAcknowledge ioAcknowledge) {
+
+        }
+
+        @Override
+        public void onMessage(JSONObject jsonObject, IOAcknowledge ioAcknowledge) {
+
+        }
+
+        @Override
+        public void on(String s, IOAcknowledge ioAcknowledge, Object... objects) {
+
+        }
+
+        @Override
+        public void onError(SocketIOException e) {
+            // TODO Auto-generated method stub
+            e.printStackTrace();
+            connectSocket();
+        }
+
+        @Override
+        public void onDisconnect() {
+            // TODO Auto-generated method stub
+            System.out.println("disconnected");
+        }
+
+    };
+
+    private Remote() {
+        this.connectSocket();
+    }
+
+    private void connectSocket() {
+        try {
+            socket = new SocketIO(url);
+        } catch (MalformedURLException e) {
+            // URL pourrie
+            e.printStackTrace();
+        }
+        System.out.println("Conneting");
+        socket.connect(this.ioCallback);
+    }
+    public static synchronized Remote getInstance() {
+        if (instance == null) {
+            instance = new Remote();
+        }
+        return instance;
+    }
+
+    public static void reset() {
+        instance = null;
+        socket = null;
+        already_connect = false;
+    }
+}
---
+++ Android/app/src/main/java/com/polytech/cluedo/WaitingActivity.java
@@ -0,0 +1,37 @@
+package com.polytech.cluedo;
+
+/**
+ * Created by Alexia on 15/01/2015.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+
+public class WaitingActivity extends Activity {
+    private TextView info_textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Remote.context = this;
+
+        Intent intent = getIntent();
+        int message = intent.getIntExtra("MESSAGE", -1);
+
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        info_textView = new TextView(this);
+        info_textView.setText((message == -1) ? R.string.waitingForPlayers : message);
+        info_textView.setGravity(Gravity.CENTER);
+        info_textView.setTextSize(30);
+
+        this.addContentView(info_textView, params);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Do Nothing
+    }
+}
