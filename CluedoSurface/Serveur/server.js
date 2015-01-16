352592581d3ec6073448cc33cfa8a96f14ee030e
mperron06 <m.perron06@gmail.com>
2015-01-16 14:02:54 +0100

    Ajout des cases de départ du joueur

--- CluedoSurface/Serveur/server.js
+++ CluedoSurface/Serveur/server.js
@@ -70,96 +70,96 @@ function init() {
     nbCases = 24;
     cases = [];
 
-    cases.push({ id: "1.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "1.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "1.5", nom: "Case", type: "couloir" });
-    cases.push({ id: "1.6", nom: "Case", type: "couloir" });
-    cases.push({ id: "2.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "2.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "2.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "2.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "2.5", nom: "Case", type: "couloir" });
-    cases.push({ id: "2.6", nom: "Case", type: "couloir" });
-    cases.push({ id: "2.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "2.8", nom: "Case", type: "couloir" });
-    cases.push({ id: "3.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "3.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "3.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "3.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "3.5", nom: "Case", type: "couloir" });
-    cases.push({ id: "3.6", nom: "Case", type: "couloir" });
-    cases.push({ id: "3.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "3.8", nom: "Case", type: "couloir" });
-    cases.push({ id: "4.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "4.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "4.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "4.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "4.5", nom: "Case", type: "couloir" });
-    cases.push({ id: "4.6", nom: "Case", type: "couloir" });
-    cases.push({ id: "4.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "4.8", nom: "Case", type: "couloir" });
-    cases.push({ id: "5.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "5.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "5.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "6.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "6.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "6.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "7.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "7.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "7.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "8.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "8.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "8.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "8.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "8.5", nom: "Case", type: "couloir" });
-    cases.push({ id: "8.6", nom: "Case", type: "couloir" });
-    cases.push({ id: "8.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "9.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "9.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "9.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "9.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "9.5", nom: "Case", type: "couloir" });
-    cases.push({ id: "9.6", nom: "Case", type: "couloir" });
-    cases.push({ id: "9.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "10.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "10.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "10.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "10.5", nom: "Case", type: "couloir" });
-    cases.push({ id: "10.6", nom: "Case", type: "couloir" });
-    cases.push({ id: "10.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "10.8", nom: "Case", type: "couloir" });
-    cases.push({ id: "11.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "11.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "11.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "11.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "11.5", nom: "Case", type: "couloir" });
-    cases.push({ id: "11.6", nom: "Case", type: "couloir" });
-    cases.push({ id: "11.7", nom: "Case", type: "couloir" });
-    cases.push({ id: "11.8", nom: "Case", type: "couloir" });
-    cases.push({ id: "12.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "12.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "12.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "12.4", nom: "Case", type: "couloir" });
-    cases.push({ id: "13.1", nom: "Case", type: "couloir" });
-    cases.push({ id: "13.2", nom: "Case", type: "couloir" });
-    cases.push({ id: "13.3", nom: "Case", type: "couloir" });
-    cases.push({ id: "13.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 0, num: "1.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 1, num: "1.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 2, num: "1.5", nom: "Case", type: "couloir" });
+    cases.push({ id: 3, num: "1.6", nom: "Case", type: "couloir" });
+    cases.push({ id: 4, num: "2.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 5, num: "2.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 6, num: "2.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 7, num: "2.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 8, num: "2.5", nom: "Case", type: "couloir" });
+    cases.push({ id: 9, num: "2.6", nom: "Case", type: "couloir" });
+    cases.push({ id: 10, num: "2.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 11, num: "2.8", nom: "Case", type: "couloir" });
+    cases.push({ id: 12, num: "3.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 13, num: "3.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 14, num: "3.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 15, num: "3.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 16, num: "3.5", nom: "Case", type: "couloir" });
+    cases.push({ id: 17, num: "3.6", nom: "Case", type: "couloir" });
+    cases.push({ id: 18, num: "3.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 19, num: "3.8", nom: "Case", type: "couloir" });
+    cases.push({ id: 20, num: "4.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 21, num: "4.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 22, num: "4.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 23, num: "4.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 24, num: "4.5", nom: "Case", type: "couloir" });
+    cases.push({ id: 25, num: "4.6", nom: "Case", type: "couloir" });
+    cases.push({ id: 26, num: "4.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 27, num: "4.8", nom: "Case", type: "couloir" });
+    cases.push({ id: 28, num: "5.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 29, num: "5.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 30, num: "5.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 31, num: "6.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 32, num: "6.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 33, num: "6.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 34, num: "7.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 35, num: "7.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 36, num: "7.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 37, num: "8.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 38, num: "8.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 39, num: "8.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 40, num: "8.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 41, num: "8.5", nom: "Case", type: "couloir" });
+    cases.push({ id: 42, num: "8.6", nom: "Case", type: "couloir" });
+    cases.push({ id: 43, num: "8.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 44, num: "9.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 45, num: "9.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 46, num: "9.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 47, num: "9.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 48, num: "9.5", nom: "Case", type: "couloir" });
+    cases.push({ id: 49, num: "9.6", nom: "Case", type: "couloir" });
+    cases.push({ id: 50, num: "9.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 51, num: "10.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 52, num: "10.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 53, num: "10.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 54, num: "10.5", nom: "Case", type: "couloir" });
+    cases.push({ id: 55, num: "10.6", nom: "Case", type: "couloir" });
+    cases.push({ id: 56, num: "10.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 57, num: "10.8", nom: "Case", type: "couloir" });
+    cases.push({ id: 58, num: "11.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 59, num: "11.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 60, num: "11.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 61, num: "11.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 62, num: "11.5", nom: "Case", type: "couloir" });
+    cases.push({ id: 63, num: "11.6", nom: "Case", type: "couloir" });
+    cases.push({ id: 64, num: "11.7", nom: "Case", type: "couloir" });
+    cases.push({ id: 65, num: "11.8", nom: "Case", type: "couloir" });
+    cases.push({ id: 66, num: "12.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 67, num: "12.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 68, num: "12.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 69, num: "12.4", nom: "Case", type: "couloir" });
+    cases.push({ id: 70, num: "13.1", nom: "Case", type: "couloir" });
+    cases.push({ id: 71, num: "13.2", nom: "Case", type: "couloir" });
+    cases.push({ id: 72, num: "13.3", nom: "Case", type: "couloir" });
+    cases.push({ id: 73, num: "13.4", nom: "Case", type: "couloir" });
 
-    cases.push({ id: "0.4", nom: "Entrée", type: "piece" });
-    cases.push({ id: "0.5", nom: "Entrée", type: "piece" });
-    cases.push({ id: "2.0", nom: "Garage", type: "piece" });
-    cases.push({ id: "3.9", nom: "Salon", type: "piece" });
-    cases.push({ id: "6.0", nom: "Salle de jeux", type: "piece" });
-    cases.push({ id: "7.8", nom: "Salle à manger", type: "piece" });
-    cases.push({ id: "11.9", nom: "Cuisine", type: "piece" });
-    cases.push({ id: "12.0", nom: "Chambre", type: "piece" });
-    cases.push({ id: "13.5", nom: "Bureau", type: "piece" });
-    cases.push({ id: "14.3", nom: "Salle de bain", type: "piece" });
+    cases.push({ id: 74, num: "0.4", nom: "Entrée", type: "piece" });
+    cases.push({ id: 75, num: "0.5", nom: "Entrée", type: "piece" });
+    cases.push({ id: 76, num: "2.0", nom: "Garage", type: "piece" });
+    cases.push({ id: 77, num: "3.9", nom: "Salon", type: "piece" });
+    cases.push({ id: 78, num: "6.0", nom: "Salle de jeux", type: "piece" });
+    cases.push({ id: 80, num: "7.8", nom: "Salle à manger", type: "piece" });
+    cases.push({ id: 81, num: "11.9", nom: "Cuisine", type: "piece" });
+    cases.push({ id: 82, num: "12.0", nom: "Chambre", type: "piece" });
+    cases.push({ id: 83, num: "13.5", nom: "Bureau", type: "piece" });
+    cases.push({ id: 84, num: "14.3", nom: "Salle de bain", type: "piece" });
 
-    cases.push({ id: "5.5", nom: "Hall", type: "hall" });
-    cases.push({ id: "6.3", nom: "Hall", type: "hall" });
-    cases.push({ id: "6.6", nom: "Hall", type: "hall" });
-    cases.push({ id: "7.5", nom: "Hall", type: "hall" });
+    cases.push({ id: 85, num: "5.5", nom: "Hall", type: "hall" });
+    cases.push({ id: 86, num: "6.3", nom: "Hall", type: "hall" });
+    cases.push({ id: 87, num: "6.6", nom: "Hall", type: "hall" });
+    cases.push({ id: 88, num: "7.5", nom: "Hall", type: "hall" });
 
     /*var test = JSON.stringify(cases);
      console.log(test);*/
@@ -194,7 +194,7 @@ function init() {
     cartes.push({ id: 20, nom: "Salle de bains", type: "piece", tag: "" });
 
 
-    cartes.push({ id: 20, nom: "Hall", type: "hall" });
+    cartes.push({ id: 21, nom: "Hall", type: "hall", tag:"" });
 
     nbAssociations = 0;
 
@@ -235,7 +235,9 @@ io.on('connection', function(socket){
         console.log(nbJoueurs);
 
         if (nbJoueurs == nbMaxJoueurs) {
-            io.sockets.emit('choixPions', joueurs);
+            //io.sockets.emit('choixPions', joueurs);
+            console.log("joueurs prets");
+            io.sockets.emit('joueursPrets', null);
         }
     });
 
@@ -274,7 +276,7 @@ io.on('connection', function(socket){
             joueurs[nbJoueurs].id = nbJoueurs;
             joueurs[nbJoueurs].name = name;
             joueurs[nbJoueurs].tag = tagPerso;
-            joueurs[nbJoueurs].numCase = 0;
+            joueurs[nbJoueurs].numCase = ["5.5", "6.3", "6.6", "7.5"]; //hall
             joueurs[nbJoueurs].supposition = [];
             joueurs[nbJoueurs].cartes = [];
             joueurs[nbJoueurs].inline = true;
@@ -284,11 +286,7 @@ io.on('connection', function(socket){
                 tableSocket.emit("nouveauJoueur", { idJoueur: nbJoueurs, pseudoJoueur: name });
             }
 
-            console.log("Avant : " + nbJoueurs);
-
             nbJoueurs++;
-
-            console.log("Apres : " + nbJoueurs);
 
             socket.emit('myId', myId);
             socket.emit('cases', cases);
@@ -321,23 +319,7 @@ io.on('connection', function(socket){
     socket.on('forcerDebutPartie', function (arg) {
         console.log(arg);
         nbMaxJoueurs = nbJoueurs;
-        io.sockets.emit('choixPions', joueurs);
-    });
-
-    socket.on('associerTag', function (arg) {
-        console.log(arg);
-        var data = JSON.parse(arg);
-        joueurs[data.idJoueur].tag = data.tag;
-        io.sockets.emit('nouveauTag', data);
-
-        nbAssociations++;
-        if (nbAssociations == nbMaxJoueurs) {
-            //io.sockets.emit('preparerPions', null);
-            // Le premier joueur est tiré aléatoirement
-            idJoueurDepart = Math.floor(Math.random() * nbMaxJoueurs);
-            idJoueurActuel = idJoueurDepart;
-            io.sockets.emit('debutPartie', { idJoueur: idJoueurActuel, idCase: joueurs[idJoueurActuel].numCase });
-        }
+        //io.sockets.emit('choixPions', joueurs);
     });
 
     socket.on('pionPret', function () {
